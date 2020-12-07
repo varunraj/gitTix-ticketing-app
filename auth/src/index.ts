e9f902c932b19ad255d95a5dc845ad5ceb709ba6
Varun Raj <varunraj82@gmail.com>
2020-12-06 23:56:54 -0500

    addeed ngnix

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,8 @@ import { json } from 'body-parser'
 const app = express();
 app.use(json());
 
-app.listen(3000, ()=> console.log("Listening to 3000"));
+app.get('/api/users/currentuser', (req,res)=>{
+    res.send('hi there');
+})
+
+app.listen(3000, ()=> console.log("Listening to 3000 !!!"));
